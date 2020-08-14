d39f84b73bf6068421e50e8f2e270947670f21d9
Luke Yan <op1982@cpcnet.local>
2020-08-14 18:00:18 +0800

    spring-demo

---
+++ readinglist/src/test/java/net/duxingzhe/luke/spring/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package net.duxingzhe.luke.spring.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-jdbc/src/main/java/net/duxingzhe/luke/spring/springjdbc/SpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package net.duxingzhe.luke.spring.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcApplication.class, args);
+	}
+
+}
