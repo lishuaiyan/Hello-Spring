e910941269fffc3f52ef2b6ca2b2d295ad8bbaab
Luke Yan <op1982@cpcnet.local>
2020-08-14 18:07:11 +0800

    add comment

--- readinglist/src/main/java/net/duxingzhe/luke/spring/readinglist/ReadingListApplication.java
+++ readinglist/src/main/java/net/duxingzhe/luke/spring/readinglist/ReadingListApplication.java
@@ -3,11 +3,17 @@ package net.duxingzhe.luke.spring.readinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author yanlishuai
+ * @SpringBootApplication 开启组件扫描和自动配置
+ */
 @SpringBootApplication
-public class ReadinglistApplication {
+public class ReadingListApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReadinglistApplication.class, args);
+        // 负责启动引导程序
+        SpringApplication.run(ReadingListApplication.class, args);
     }
 
 }
